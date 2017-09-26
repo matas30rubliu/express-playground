f6f266e30f4711c5f107c5e6c0c9fd4f22071832
Matas <matas30rubliu@gmail.com>
2017-09-26 23:08:30 +0300

    Chat removed, cleaning unused files

--- server/pavezu.js
+++ server/pavezu.js
@@ -1,7 +1,4 @@
-const path = require('path');
-const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const OauthStrategy = require('passport-google-oauth20').Strategy;
@@ -10,20 +7,10 @@ const keys = require('../config/keys.js');
 const utils = require('./utils.js');
 
 const portForHeroku = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, '../public');
 const moment = require('moment');
 
 // API reference for HTTP server: https://expressjs.com/
-// API reference for server that support web sockets: https://socket.io/docs/
-var app = express();
-// http.createServer() is called by express with app.listen. To configure socket.io, some modifications have to be made.
-// By defining this server var we swap app.listen with server.listen.
-var server = http.createServer(app);
-// Getting web sockets server (it will allow to accept new connections):
-var io = socketIO(server);
-// Check .../socket.io/socket.io.js - it is library which contains code needed for client to connect to server and transfer data.
-
-app.use(express.static(publicPath));
+const app = express();
 
 // passport can keep track of auth flow in many ways. E.g. by cookies.
 // By default exress does not know about cookies, init that:
@@ -36,27 +23,6 @@ app.use(cookieSession({
 // Use cookies to handle auth
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Registering listener for server. 'connection' is built-in event
-io.on('connection', (socket) => {
-  console.log('User with socket id %s connected to server', socket.id);
-
-  socket.on('createMessage', (clientMessage) => {
-    // When specific client sends new message, publish it to all clients
-    io.emit('newMessage', utils.constructMessage(clientMessage.from, clientMessage.message));
-
-    // Broadcasting is similar to io.emit, but socket which emits does not receive its own message
-    // socket.broadcast.emit('newMessage', {
-    //   from: socket.id,
-    //   message: clientMessage.message
-    // });
-
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User with socket id %s disconnected from server', socket.id);
-  });
-});
 
 passport.serializeUser((user, done) => {
   // first param is error object (null because we expect no errors)
@@ -117,10 +83,10 @@ if (process.env.NODE_ENV === 'production') {
     // serve index.html if route requested is supposed to be handled by react
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-server.listen(portForHeroku, () => {
+app.listen(portForHeroku, () => {
   console.log(`Server started on port ${portForHeroku}`);
 });
